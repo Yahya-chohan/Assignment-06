617086b2e4972b5af9514e0877748bc8eba510ab
Yahya chohan <yahyachohan123@gmail.com>
2025-02-08 19:07:00 +0500

    add js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 let result = document.getElementById("result")
-function genotp() {
-    let otp = Math.round(Math.random()*1000000)
-result.innerText = `Your OTP Code is ${otp}` 
-console.log(otp)
+function genotp(){
+    let otp = Math.round(Math.random()*100000)
+    result.innerText = `Your OTP is ${otp}`
+    console.log(otp)
 }
